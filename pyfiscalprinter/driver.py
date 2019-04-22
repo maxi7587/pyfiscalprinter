915cb71298702de0e756b9d3f62686e9aea48e85
maxi7587 <maxi7587@hotmail.com>
2019-04-22 10:46:41 -0300

    working in ord

--- pyfiscalprinter/driver.py
+++ pyfiscalprinter/driver.py
@@ -84,7 +84,7 @@ class EpsonFiscalDriver:
 
     def _read( self, count ):
         ret = self._serialPort.read( count )
-        debug( "_read", ", ".join( [ "%x" % ord(c) for c in ret ] ) )
+        debug( "_read", ", ".join( [ "%x" % (ord(c) if type(c) == 'str' else c) for c in ret ] ) )
         return ret
 
     def __del__( self ):
