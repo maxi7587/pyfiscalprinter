2cd056e03f4293e791a53f25b4bb5fa32ceb1893
maxi7587 <maxi7587@hotmail.com>
2019-04-22 12:35:08 -0300

    working in ord

--- pyfiscalprinter/driver.py
+++ pyfiscalprinter/driver.py
@@ -212,6 +212,9 @@ class EpsonFiscalDriver:
                 print('--------------------------------------')
                 print('read BCC', bcc)
                 print('--------------------------------------')
+                print('seq number comparison --->', reply[1] != chr( self._sequenceNumber ))
+                print('reply[1] --->', reply[1])
+                print('chr( self._sequenceNumber) --->', chr( self._sequenceNumber))
                 if not self._checkReplyBCC( reply, bcc ):
                     print('-----------------------------------------')
                     print('not _checkReplyBCC, incrementing timeout')
@@ -223,9 +226,6 @@ class EpsonFiscalDriver:
                     if retries > self.RETRIES:
                         raise ComunicationError("Fallo de comunicación, demasiados paquetes inválidos (bad bcc).")
                     continue
-                print('seq number comparison --->', reply[1] != chr( self._sequenceNumber ))
-                print('reply[1] --->', reply[1])
-                print('chr( self._sequenceNumber) --->', chr( self._sequenceNumber))
                 elif reply[1] != chr( self._sequenceNumber ): # Los número de seq no coinciden
                     print('-----------------------------------------')
                     print('seq number do not match, incrementing timeout')
