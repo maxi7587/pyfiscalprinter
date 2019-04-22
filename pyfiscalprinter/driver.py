2b6e4109d2cd992c8f8d36725c7a9147b22e312a
maxi7587 <maxi7587@hotmail.com>
2019-04-22 12:49:11 -0300

    working in ord

--- pyfiscalprinter/driver.py
+++ pyfiscalprinter/driver.py
@@ -125,8 +125,8 @@ class EpsonFiscalDriver:
 
     def _parseReply( self, reply, skipStatusErrors ):
         r = reply[4:-1] # Saco STX <Nro Seq> <Nro Comando> <Sep> ... ETX
-        print('_parseReply r', r)
-        fields = r.split( chr(28) )
+        print('_parseReply r', r.encode())
+        fields = r.encode().split( chr(28) )
         print('_parseReply fields', fields)
         printerStatus = fields[0]
         fiscalStatus = fields[1]
